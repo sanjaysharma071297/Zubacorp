d52416c782b074f2e5e8b08b01eccf95a1684d3b
sanjaysharma071297 <68582651+sanjaysharma071297@users.noreply.github.com>
2023-07-30 10:29:31 +0530

    Update pymongo_get_database.py

--- pymongo_get_database.py
+++ pymongo_get_database.py
@@ -4,10 +4,10 @@ def get_database():
    # Provide the mongodb atlas url to connect python to mongodb using pymongo
    connection_string = "<provide_your_connection_string>"
 
-   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   # Create a connection using MongoClient.
    client = MongoClient(connection_string)
 
-   # Create the database for our example (we will use the same database throughout the tutorial
+   # Create the database 
    return client['zaubacorp_company_data']
   
 # This is added so that many files can reuse the function get_database()
